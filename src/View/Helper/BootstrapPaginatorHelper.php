d69467988fb81007642f5a3b98f43289bcb3cbcf
Niklas Rother <info@niklas-rother.de>
2015-02-17 19:27:30 +0100

    Paginator: Remove overloaded numbers().

--- src/View/Helper/BootstrapPaginatorHelper.php
+++ src/View/Helper/BootstrapPaginatorHelper.php
@@ -24,15 +24,15 @@ namespace Bootstrap3\View\Helper;
 
 use Cake\View\Helper\PaginatorHelper;
 
-class BootstrapPaginatorHelper extends PaginatorHelper {
-
-
-    public function __construct ($view, $config = []) {
+class BootstrapPaginatorHelper extends PaginatorHelper
+{
+    public function __construct ($view, $config = [])
+    {
         $this->templates([
-            'nextActive' => '<li><a href="{{url}}">{{text}}</a></li>',
-            'nextDisabled' => '<li class="disabled"><a>{{text}}</a></li>',
-            'prevActive' => '<li><a href="{{url}}">{{text}}</a></li>',
-            'prevDisabled' => '<li class="disabled"><a>{{text}}</a></li>',
+            'nextActive' => '<li><a href="{{url}}" aria-label="{{text}}"><span aria-hidden="true">&raquo;</span></a></li>',
+            'nextDisabled' => '<li class="disabled"><a href="" aria-label="{{text}}"><span aria-hidden="true">&raquo;</span></a></li>',
+            'prevActive' => '<li><a href="{{url}}" aria-label="{{text}}"><span aria-hidden="true">&laquo;</span></a></li>',
+            'prevDisabled' => '<li class="disabled"><a href="" aria-label="{{text}}"><span aria-hidden="true">&laquo;</span></a></li>',
             'first' => '<li><a href="{{url}}">{{text}}</a></li>',
             'last' => '<li><a href="{{url}}">{{text}}</a></li>',
             'number' => '<li><a href="{{url}}">{{text}}</a></li>',
@@ -41,58 +41,5 @@ class BootstrapPaginatorHelper extends PaginatorHelper {
         
         parent::__construct($view, $config);
     }
-    
-    /**
-     * 
-     * Get pagination link list.
-     * 
-     * @param $options Options for link element
-     *
-     * Extra options:
-     *  - size small/normal/large (default normal)
-     *       
-    **/
-    public function numbers (array $options = array()) {       
-        
-        $class = 'pagination' ;
-
-        if (isset($options['class'])) {
-            $class .= ' '.$options['class'] ;
-            unset($options['class']) ;
-        }
-        
-        if (isset($options['size'])) {
-            switch ($options['size']) {
-            case 'small':
-                $class .= ' pagination-sm' ;
-                break ;
-            case 'large':
-                $class .= ' pagination-lg' ;
-                break ;
-            }
-            unset($options['size']) ;
-        }
-          
-        if (!isset($options['before'])) {
-            $options['before'] = '<ul class="'.$class.'">' ;
-        }
-        
-        if (!isset($options['after'])) {
-            $options['after'] = '</ul>' ;
-        }
-
-        if (isset($options['prev'])) {
-            $options['before'] .= $this->prev($options['prev']) ;
-        }
-
-        if (isset($options['next'])) {
-            $options['after'] = $this->next($options['next']).$options['after'] ;
-        }
-                
-        return parent::numbers ($options) ;
-    }
-
-
 }
-
 ?>
